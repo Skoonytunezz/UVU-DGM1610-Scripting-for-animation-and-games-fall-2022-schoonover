9c4b45658a0693bd28d60bd51e1171ba46b4641a
Brett Schoonover <skoonytunezz@gmail.com>
2022-09-25 19:48:26 -0600

    started a new project UFO Game

---
+++ UFO Game/Assets/Scriptes/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float hInput;
+    public float speed;
+
+    public float xRange = 11.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set horizontal input to recieve values from the keyboard keymap horizontal
+        hInput = Input.GetAxis("Horizontal");
+
+        //Move player left and right
+        transform.Translate(Vector3.right * hInput * speed * Time.deltaTime);
+
+        //Keep player within set bounds
+        //keep player inside right wall at set xRange
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // keep player inside left wall at set -xRange
+         if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
